53dd30e4be947828313f53eb150b0bb04c39db6c
SeraNoxaEAMV <72008720+SeraNoxaEAMV@users.noreply.github.com>
2021-11-01 15:12:08 +0100

    front-end models are made along with a pipe.

---
+++ BlogLab-UI/src/app/Models/blog-comment/blog-comment-create.model.ts
@@ -0,0 +1,11 @@
+export class BlogCommentCreate {
+
+    constructor(
+        public blogCommentId: number,
+        public blogId: number,
+        public content: string,
+        public ParentblogCommentId: number
+
+    ){}
+    
+}
--- BlogLab.Models/Account/ApplicationUserLogin.cs
+++ BlogLab.Models/Account/ApplicationUserLogin.cs
@@ -8,13 +8,13 @@ namespace BlogLab.Models.Account
     public class ApplicationUserLogin
     {
         [Required(ErrorMessage = "Username is required")]
-        [MinLength(5, ErrorMessage ="Must be 5-20 characters")]
-        [MaxLength(5, ErrorMessage = "Must be 5-20 characters")]
+        [MinLength(5, ErrorMessage ="Must be 5-30 characters")]
+        [MaxLength(30, ErrorMessage = "Must be 5-30 characters")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [MinLength(8, ErrorMessage = "Must be 8-20 characters")]
-        [MaxLength(8, ErrorMessage = "Must be 8-20 characters")]
+        [MaxLength(20, ErrorMessage = "Must be 8-20 characters")]
         public string Password { get; set; }
 
 
